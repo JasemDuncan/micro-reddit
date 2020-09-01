d986cc512300869f489c6293c93b076c4efecaee
JasemDuncan <jasemvalencia@gmail.com>
2020-09-01 11:59:09 -0500

    Follow the requirements of micro-reedit pr

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+    belongs_to :user
+    has_many :comments
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :date, presence: true
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    has_many :posts
+    has_many :comments
+    validates :name, presence: true
+    validates :age, presence: true
+    validates :country, presence: true
+
+end
